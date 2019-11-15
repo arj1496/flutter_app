3cd489a0ef815388e577b5158f9cf42f4e17139c
Urvesh Rathod <urvesh.educloud@gmail.com>
2019-11-15 11:11:08 +0530

    init

--- lib/NewsFeed.dart
+++ lib/NewsFeed.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/UrlUtils.dart';
 import 'package:flutter_app/src/mo/Exam/ExamService.dart';
 import 'package:flutter_app/src/mo/NewsFeed/WevViewActivity.dart';
+import 'package:flutter_app/src/mo/TimeTable/CounterStorage.dart';
 import 'package:path_provider/path_provider.dart';
 
 class NewsFeed extends StatefulWidget {
@@ -61,43 +62,9 @@ void getFileFromServer() async{
   UrlUtils urlUtils = new UrlUtils();
   HashMap map = new HashMap<String, String>();
   String url=  urlUtils.getNewsFeedUrl();
-  Map<String, dynamic> webViewFromServer = await  webViewActivity.getData_(map, url);
-  var files = webViewFromServer["files"];
+ /* Map<String, dynamic> webViewFromServer = await  webViewActivity.getData(map, url);
+  var files = webViewFromServer["files"];*/
 
   String test = "123";
   //getData_
-}
-
-class CounterStorage {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/counter.txt');
-  }
-
-  Future<int> readCounter() async {
-    try {
-      final file = await _localFile;
-
-      // Read the file
-      String contents = await file.readAsString();
-
-      return int.parse(contents);
-    } catch (e) {
-      // If encountering an error, return 0
-      return 0;
-    }
-  }
-
-  Future<File> writeCounter(int counter) async {
-    final file = await _localFile;
-
-    // Write the file
-    return file.writeAsString('$counter');
-  }
 }
